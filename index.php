3efdad19ad0933315b95aea61d05be9e8b4cda2a
Benson Muite <benson_muite@emailplus.org>
2017-10-29 12:34:08 +0200

    readded index file

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet"
+  type="text/css" href="https://fonts.googleapis.com/css?family=Raleway" />
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<title>Seniorship</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+<style>
+#Otsingruba {
+    border: solid 1px #CCC;
+    height: 21px;
+    width: 250px;
+    font-size: 15px;
+    color: #999;
+    padding-left: 5px;
+    vertical-align: bottom
+}
+
+#Otsi {
+    border: solid 1px #CCC;
+    height: 25px;
+    width: 115px;
+    color: #556978;
+}
+</style>
+ </head>
+<body>
+<ul>
+  <li style="background-color:#46cdc4"><a href="liitu.php">Liitu</a></li>
+<li><a href="praktikandile.php">Praktikandile</a></li>
+<li><a href="firmale.php">Firmale</a></li>
+<li><a href="index.php">Pakkumised</a></li>
+<li style="float:left"><a href="index.php"> <img src="logo.png" alt="Seniorship" style="height:100px;"></a></li>
+</ul>
+
+ <h1>Kuidas see toimib?</h1>
+
+ <table align="center" style="width:100%; background-color:#fff">
+<tr>
+   <th> <img src="magnifying-glass-search-button.png" alt="Leia praktika" style="width:30px; height:30px;">
+   </th>
+   <th> <img src="Kandideeri.png" alt="Kandideeri" style="width:30px; height:30px;">
+    </th>
+   <th> <img src="wrench.png" alt="Tööta" style="width:30px; height:30px;">
+    </th>
+   <th> <img src="euro.png" alt="Teeni raha" style="width:40px; height:40px;">
+    </th>
+ </tr>
+ <tr>
+<<<<<<< HEAD
+  <th><h3>LEIA PRAKTIKA</h3></th>
+=======
+  <th><h3>LEIA PRAKTIKA</h3> <div align="center" style="width:10px; height:2px;border:1px solid #000;"></div></th>
+>>>>>>> d68974b664bf90ca1f74ee531e90cf72a93c5d12
+   <th><h3>KANDIDEERI</h3></th>
+   <th><h3>TÖÖTA</h3></th>
+   <th><h3>TEENI RAHA</h3></th>
+ </tr>
+ <tr>
+  <th> <img src="ruba.png" alt="Ruba" style="height:20px;"></th>
+   <th> <img src="ruba.png" alt="Ruba" style="height:20px;"></th>
+   <th> <img src="ruba.png" alt="Ruba" style="height:20px;"></th>
+   <th> <img src="ruba.png" alt="Ruba" style="height:20px;"></th>
+ </tr>
+ <tr>
+   <td>Vali meie pakkumiste</td>
+  <td>Esita oma kandidatuur</td>
+   <td>Alusta oma uut</td>
+      <td>Sinu kontole laekub</td>
+</tr>
+<tr>
+  <td>hulgast huvitav praktika</td>
+ <td>kõigest mõne minutiga</td>
+  <td>ja huvitavat praktikat!</td>
+     <td>kuu lõpus praktikatasu</td>
+   </tr>
+ </table>
+ <br>
+ <h1> Praktikapakkumised</h1>
+  <p> <div align="center" style="color:#556978">
+  Sisestage märksõna, et otsida praktikapakkumisi või sirvige kõiki allpool olevaid pakkumisi. </div></p>
+<form>
+  <textarea id="Otsingruba" rows="1" cols="50" name="Otsinguriba">
+  </textarea>
+     <input id="Otsi" type="submit" value="Otsi" align="center" >
+</form>
+<br>
+
+<table align="center" style="background-color:#eee; width:70%;">
+
+  <tr>
+    <th><h2>Pealkiri</h2></th>
+    <th>Tasu</th>
+    <th>Praktika pikkus</th>
+    <th>Asukoht</th>
+    <th>Loe edasi</th>
+  </tr>
+</table>
+
+
+</body>
+</html>
